7aff5f4f7058700133513a0b09717943545c8652
GodFox <zhangliuxian@gmail.com>
2022-03-01 16:48:33 +0800

    Update rp-helper.sh

--- rp-helper.sh
+++ rp-helper.sh
@@ -107,7 +107,7 @@ function runContainer(){
             BINDS+="--volume $(realpath "${HOST_PATH}"):${CONTAINER_PATH} "
         done
     fi
-    docker run --privileged --rm  $( [ "${CMD}" == "run" ] || [ "${CMD}" == "ext" ] && echo " --interactive") --tty \
+    docker run --privileged --rm  $( [ "${CMD}" == "run" ] || [ "${CMD}" == "ext" ] && echo " --interactive") \
         --name rp-helper \
         --hostname rp-helper \
         --volume /dev:/dev \
